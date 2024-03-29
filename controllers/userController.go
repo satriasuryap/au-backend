8b8f9808336351124297ecae71a754f85362beac
satriasuryap <satriasurya0708@gmail.com>
2024-03-29 14:41:22 +0800

    completed userController

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"golang-au-backend/database"
+	helper "golang-au-backend/helpers"
 	"golang-au-backend/models"
 	"log"
 	"net/http"
@@ -11,12 +12,15 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -86,6 +90,46 @@ func SignUp() gin.HandlerFunc {
 		}
 
 		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error on retrieveing email"})
+			return
+		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email already exist"})
+			return
+		}
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
+
+		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, user.User_id)
+		user.Token = &token
+		user.Refresh_Token = &refreshToken
+
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		//return status OK and send the result back
+
+		c.JSON(http.StatusOK, resultInsertionNumber)
 	}
 }
 
--- models/userModel.go
+++ models/userModel.go
@@ -11,11 +11,12 @@ type User struct {
 	Username      string             `json:"username" gorm:"unique" bson:"username,omitempty"`
 	First_name    *string            `json:"first_name" validate:"required,min=2,max=100"`
 	Last_name     *string            `json:"last_name" validate:"required,min=2,max=100"`
-	Email         string             `json:"email" gorm:"unique" bson:"email,omitempty"`
-	Password      []byte             `json:"password" bson:"password"`
+	Email         *string            `json:"email" validate:"email,required"`
+	Password      *string            `json:"password" bson:"password"`
 	Token         *string            `json:"token"`
 	Refresh_Token *string            `json:"refresh_token"`
 	CreatedAt     time.Time          `json:"createdat" bson:"createat"`
-	DeactivatedAt time.Time          `json:"updatedat" bson:"updatedat"`
+	DeactivatedAt time.Time          `json:"deactivateat" bson:"deactivateat"`
+	UpdatedAt     time.Time          `json: "updatedat" bson:"updatedat"`
 	User_id       string             `json:"user_id"`
 }
